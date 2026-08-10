dc7ee9823d34375b599d5810e68ad9df39e229b0
rodrigo <rodrilasek02@gmail.com>
2026-08-10 17:01:02 -0300

    fase 3

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         do {
 
-            System.out.println("\n===== MENU =====");
+            System.out.println("\n===== BEM VINDO AO PDV =====");
             System.out.println("1 - Cadastrar Gerente");
             System.out.println("2 - Cadastrar Operador de Caixa");
             System.out.println("3 - Realizar Pagamento");
@@ -84,29 +84,175 @@ public class Main {
 
                     loja.adicionarFuncionario(operador);
 
-                    System.out.println("Operador cadastrado com sucesso!");
-
                     break;
 
                 case 3:
 
-                    Pagamento pagamento = new Pagamento();
+                    System.out.println("\n===== FORMA DE PAGAMENTO =====");
+                    System.out.println("1 - PIX");
+                    System.out.println("2 - Boleto");
+                    System.out.println("3 - Cartao");
+                    System.out.print("Escolha uma opcao: ");
 
-                    System.out.print("Numero do Pagamento: ");
-                    pagamento.setNumeroPagamento(sc.nextInt());
-
-                    System.out.print("Valor da Venda: ");
-                    pagamento.setValorVenda(sc.nextDouble());
+                    int formaPagamento = sc.nextInt();
                     sc.nextLine();
 
-                    System.out.print("Modalidade de Pagamento: ");
-                    pagamento.setModalidadePagamento(sc.nextLine());
+                    switch (formaPagamento) {
 
-                    pagamento.realizarPagamento();
+                        case 1:
 
-                    if ("APROVADO".equals(pagamento.getStatus())) {
-                        loja.cadastrarPagamento(pagamento);
-                        System.out.println("Pagamento aprovado!");
+                            Pix pix = new Pix();
+
+                            System.out.print("Numero do Pagamento: ");
+                            pix.setNumeroPagamento(sc.nextInt());
+
+                            System.out.print("Valor: ");
+                            pix.setValor(sc.nextDouble());
+                            sc.nextLine();
+
+                            System.out.print("Chave PIX: ");
+                            pix.setChavePix(sc.nextLine());
+
+                            pix.realizarPagamento();
+
+                            if ("APROVADO".equals(pix.getStatus())) {
+                                loja.adicionarPagamento(pix);
+                            }
+
+                            break;
+
+                        case 2:
+
+                            Boleto boleto = new Boleto();
+
+                            System.out.print("Numero do Pagamento: ");
+                            boleto.setNumeroPagamento(sc.nextInt());
+
+                            System.out.print("Valor: ");
+                            boleto.setValor(sc.nextDouble());
+                            sc.nextLine();
+
+                            System.out.print("Codigo de Barras: ");
+                            boleto.setCodigoBarras(sc.nextLine());
+
+                            boleto.realizarPagamento();
+
+                            if ("APROVADO".equals(boleto.getStatus())) {
+                                loja.adicionarPagamento(boleto);
+                            }
+
+                            break;
+
+                        case 3:
+
+                            System.out.println("\n===== TIPO DE CARTAO =====");
+                            System.out.println("1 - Debito");
+                            System.out.println("2 - Credito");
+                            System.out.print("Escolha uma opcao: ");
+
+                            int tipoCartao = sc.nextInt();
+                            sc.nextLine();
+
+                            switch (tipoCartao) {
+
+                                case 1:
+
+                                    CartaoDebito debito = new CartaoDebito();
+
+                                    System.out.print("Numero do Pagamento: ");
+                                    debito.setNumeroPagamento(sc.nextInt());
+
+                                    System.out.print("Valor: ");
+                                    debito.setValor(sc.nextDouble());
+                                    sc.nextLine();
+
+                                    System.out.print("Numero do Cartao: ");
+                                    debito.setNumeroCartao(sc.nextLine());
+
+                                    System.out.print("Nome do Titular: ");
+                                    debito.setNomeTitular(sc.nextLine());
+
+                                    System.out.print("Bandeira: ");
+                                    debito.setBandeira(sc.nextLine());
+
+                                    System.out.print("CVV: ");
+                                    debito.setCvv(sc.nextLine());
+
+                                    System.out.print("Saldo Disponivel: ");
+                                    debito.setSaldoDisponivel(sc.nextDouble());
+                                    sc.nextLine();
+
+                                    System.out.print("Senha: ");
+                                    debito.setSenha(sc.nextLine());
+
+                                    debito.realizarPagamento();
+
+                                    if ("APROVADO".equals(debito.getStatus())) {
+                                        loja.adicionarPagamento(debito);
+                                    }
+
+                                    break;
+
+                                case 2:
+
+                                    System.out.print("Pagamento a vista ou parcelado? (1 - A vista / 2 - Parcelado): ");
+                                    int tipoPagamento = sc.nextInt();
+
+                                    CartaoCredito credito;
+
+                                    if (tipoPagamento == 2) {
+
+                                        System.out.print("Quantidade de parcelas: ");
+                                        int parcelas = sc.nextInt();
+
+                                        credito = new CartaoCredito(parcelas);
+
+                                    } else {
+
+                                        credito = new CartaoCredito();
+                                    }
+
+                                    System.out.print("Numero do Pagamento: ");
+                                    credito.setNumeroPagamento(sc.nextInt());
+
+                                    System.out.print("Valor: ");
+                                    credito.setValor(sc.nextDouble());
+                                    sc.nextLine();
+
+                                    System.out.print("Numero do Cartao: ");
+                                    credito.setNumeroCartao(sc.nextLine());
+
+                                    System.out.print("Nome do Titular: ");
+                                    credito.setNomeTitular(sc.nextLine());
+
+                                    System.out.print("Bandeira: ");
+                                    credito.setBandeira(sc.nextLine());
+
+                                    System.out.print("CVV: ");
+                                    credito.setCvv(sc.nextLine());
+
+                                    System.out.print("Limite Disponivel: ");
+                                    credito.setLimiteDisponivel(sc.nextDouble());
+                                    sc.nextLine();
+
+                                    credito.realizarPagamento();
+
+                                    if ("APROVADO".equals(credito.getStatus())) {
+                                        loja.adicionarPagamento(credito);
+                                    }
+
+                                    break;
+
+                                default:
+
+                                    System.out.println("Opcao invalida.");
+                            }
+
+                            break;
+
+                        default:
+
+                            System.out.println("Opcao invalida.");
                     }
 
                     break;
